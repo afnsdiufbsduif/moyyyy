3be0d63e88dcd2486920e6cb45db40f3e84963d2
Artem <ilyaka1012@gmail.com>
2024-05-02 10:53:57 +0300

    Добавили метод InputArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string[] InputArray()
+    {
+        Console.Write("Введите размер массива строк: ");
+        int size = int.Parse(Console.ReadLine());
+        string[] arr = new string[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            arr[i] = Console.ReadLine();
+        }
+        return arr;
+    }
